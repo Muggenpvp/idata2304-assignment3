58f44ff4d27183b8671463fb8c47a0c7b340c7ab
Magnus Pedersen Voldsund <magnus.voldsund@outlook.com>
2025-09-24 19:54:13 +0200

    Started implementing the SmartTV's functions and added a server file

--- kode/smart_tv.py
+++ kode/smart_tv.py
@@ -1,1 +1,21 @@
-import socket
+class SmartTV():
+    isTurnedOn = False
+    selectedChannel = 1
+
+    def __init__(self, amountOfChannels):
+        self.amountOfChannels = amountOfChannels
+
+    def isOn(self):
+        return self.isTurnedOn
+    
+    def getAmountOfChannels(self):
+        return self.amountOfChannels
+    
+    def setSelectedChannel(self, channel):
+        if channel not in range(1,self.amountOfChannels):
+            return f"Channel {channel} does not exist"
+
+        self.selectedChannel = channel
+
+    def getSelectedChannel(self):
+        return self.selectedChannel
